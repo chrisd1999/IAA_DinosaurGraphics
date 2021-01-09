47bff2640f93a68d144842682a5d7d7975fa3598
Kristers <45801982+chrisd1999@users.noreply.github.com>
2021-01-09 13:18:03 +0200

    NonLocalMeansFilter implementation
    NonLocalMeansFilter implementation

--- filters/NonLocalMeansFilter.cs
+++ filters/NonLocalMeansFilter.cs
@@ -4,169 +4,124 @@ using static DinosaurGraphics.helpers.GuassianKernelHelper;
 namespace DinosaurGraphics.filters {
     public static class NonLocalMeansFilter {
 
-        public static PixelRGB[,] NonLocalMeansImpl(PixelRGB[,] src, PixelRGB[,] dest, double h, int window_size, int sim_window_size) {
+        public static PixelRGB[,] NonLocalMeansImpl(PixelRGB[,] src, PixelRGB[,] dest, double h, int mid_patch, int mid_sim) {
 
             int width = src.GetLength(0);
             int height = src.GetLength(1);
 
-            double[,] kernel = GuassianKernel(window_size, 1);
-            int mid_window = (window_size - 1) / 2;
-            int mid_sim_window = (sim_window_size - 1) / 2;
+
+            int patch_size = 2*mid_patch + 1;
+            double[,] kernel = GuassianKernel(patch_size-1, 1);
 
             double h2 = h * h;
 
-            for(int i = mid_window; i < width - mid_window; i++) {
-                for(int j = mid_window; j < height - mid_window; j++) {
+            for(int i = mid_patch; i < width - mid_patch; i++) {
+                for(int j = mid_patch; j < height - mid_patch; j++) {
 
-                    PixelRGB[,] W1 = GetWindow(src, i - mid_window, i + mid_window, j - mid_window, j + mid_window);
+                    byte[,] W1 = GetWindow(src, i - mid_patch, i + mid_patch, j - mid_patch, j + mid_patch);
 
-                    int umin = Math.Max(i - mid_sim_window, mid_window+1);
-                    int umax = Math.Min(i + mid_sim_window, width - mid_window);
-                    int vmin = Math.Max(j - mid_sim_window, mid_window+1);
-                    int vmax = Math.Min(j + mid_sim_window, height - mid_window);
+                    int umin = Math.Max(i - mid_sim, mid_patch + 1);
+                    int umax = Math.Min(i + mid_sim, width - mid_patch);
+                    int vmin = Math.Max(j - mid_sim, mid_patch + 1);
+                    int vmax = Math.Min(j + mid_sim, height - mid_patch);
 
-                    double Zr = 0.0d;
-                    double Zg = 0.0d;
-                    double Zb = 0.0d;
-
+                    double Z = 0.0d;
                     double NLr = 0.0d;
                     double NLg = 0.0d;
                     double NLb = 0.0d;
 
+
                     for (int u = umin; u < umax; u++) {
-                        for (int v = vmin; v < vmax; v++) {
+                        for(int v = vmin; v < vmax; v++) {
 
-                            PixelRGB[,] W2 = GetWindow(src, u - mid_window, u + mid_window, v - mid_window, v + mid_window);
+                            byte[,] W2 = GetWindow(src, u - mid_patch, u + mid_patch, v - mid_patch, v + mid_patch);
 
-                            var result_window = SubstractWindows(W1, W2, window_size - 1);
+                            double distance2 = WeightedDistance(W1, W2, kernel, patch_size-1);
 
-                            var norm_value = Normalize(result_window, window_size - 1);
+                            double weight = Math.Exp(-distance2 / (3* h2));
+                            
+                            NLr += (weight * src[u, v].R);
+                            NLg += (weight * src[u, v].G);
+                            NLb += (weight * src[u, v].B);
 
-                            double sijR = Math.Exp(-norm_value.Item1 / h2);
-                            double sijG = Math.Exp(-norm_value.Item2 / h2);
-                            double sijB = Math.Exp(-norm_value.Item3 / h2);
 
-                            //var D2 = WeightedDistance(W1, W2, kernel, window_size - 1);
-
-                            //var sumDistance = SumDistance(D2, window_size - 1);
-                            //double sijR = Math.Exp(-sumDistance.Item1 / h2);
-                            //double sijG = Math.Exp(-sumDistance.Item2 / h2);
-                            //double sijB = Math.Exp(-sumDistance.Item3 / h2);
-
-                            Zr += sijR;
-                            Zg += sijG;
-                            Zb += sijB;
-
-                            NLr += (sijR * src[i, j].R);
-                            NLg += (sijG * src[i, j].G);
-                            NLb += (sijB * src[i, j].B);
+                            Z += weight;
                         }
                     }
 
-                    dest[i, j].R = (byte)Math.Max(0, Math.Min(255, NLr / Zr));
-                    dest[i, j].G = (byte)Math.Max(0, Math.Min(255, NLg / Zg));
-                    dest[i, j].B = (byte)Math.Max(0, Math.Min(255, NLb / Zb));
+                    dest[i, j].R = (byte)Math.Max(0, Math.Min(255, NLr / Z));
+                    dest[i, j].G = (byte)Math.Max(0, Math.Min(255, NLg / Z));
+                    dest[i, j].B = (byte)Math.Max(0, Math.Min(255, NLb / Z));
+
                 }
             }
 
             return dest;
         }
 
-        static Tuple<double, double, double> Normalize(Tuple<int[,], int[,], int[,]> values, int size) {
+        static double WeightedDistance(byte[,] W1, byte[,] W2, double[,] kernel, int size) {
 
-            double sumR = 0.0d;
-            double sumG = 0.0d;
-            double sumB = 0.0d;
+            double[,] distance = new double[size, size];
 
-            for(int i = 0; i < size; i++) {
-                for(int j = 0; j < size; j++) {
-                    sumR += Math.Pow(values.Item1[i, j], 2.0);
-                    sumG += Math.Pow(values.Item2[i, j], 2.0);
-                    sumB += Math.Pow(values.Item3[i, j], 2.0);
-                }
-            }
+            int[,] substracted = SubstractWindows(W1, W2, size);
+            int[,]  multiplied = MultiplyWindows(substracted, substracted, size);
 
-            return Tuple.Create(sumR, sumG, sumB);
-        }
-
-        static Tuple<double[,], double[,], double[,]> WeightedDistance(PixelRGB[,] W1, PixelRGB[,] W2, double[,] kernel, int size) {
-
-            double[,] distanceR = new double[size, size];
-            double[,] distanceG = new double[size, size];
-            double[,] distanceB = new double[size, size];
-
-            var substracted = SubstractWindows(W1, W2, size);
-            var multiplied = MultiplyWindows(substracted, size);
-
-            for(int i = 0; i < size; i++) {
-                for(int j = 0; j < size; j++) {
-                    distanceR[i, j] = multiplied.Item1[i, j] * kernel[i, j];
-                    distanceG[i, j] = multiplied.Item2[i, j] * kernel[i, j];
-                    distanceB[i, j] = multiplied.Item3[i, j] * kernel[i, j];
-                }
-            }
-
-            return Tuple.Create(distanceR, distanceG, distanceB);
-        }
-
-        static Tuple<double, double, double> SumDistance(Tuple<double[,], double[,], double[,]> distance, int size) {
-            double sumR = 0.0d;
-            double sumG = 0.0d;
-            double sumB = 0.0d;
-
-            for(int i = 0; i < size; i++) {
-                for(int j = 0; j < size; j++) {
-                    sumR += distance.Item1[i, j];
-                    sumG += distance.Item2[i, j];
-                    sumB += distance.Item3[i, j];
-                }
-            }
-
-            return Tuple.Create(sumR, sumG, sumB);
-        }
-
-        static Tuple<int[,], int[,], int[,]> SubstractWindows(PixelRGB[,] v, PixelRGB[,] u, int size) {
-
-            int[,] substractedR = new int[size, size];
-            int[,] substractedG = new int[size, size];
-            int[,] substractedB = new int[size, size];
-
-
-            for (int i = 0; i < size; i++) {
-                for(int j = 0; j < size; j++) {
-                    substractedR[i, j] = v[i, j].R - u[i, j].R;
-                    substractedG[i, j] = v[i, j].R - u[i, j].G;
-                    substractedB[i, j] = v[i, j].R - u[i, j].B;
-                }
-            }
-
-            return Tuple.Create(substractedR, substractedG, substractedB);
-        }
-
-        static Tuple<int[,], int[,], int[,]> MultiplyWindows(Tuple<int[,], int[,], int[,]> items, int size) {
-
-            int[,] multipliedR = new int[size, size];
-            int[,] multipliedG = new int[size, size];
-            int[,] multipliedB = new int[size, size];
-
+            /* Convulation Kernel */
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < size; j++) {
-                    multipliedR[i, j] = items.Item1[i, j] * items.Item1[i, j];
-                    multipliedG[i, j] = items.Item2[i, j] * items.Item2[i, j];
-                    multipliedB[i, j] = items.Item3[i, j] * items.Item3[i, j];
+                    distance[i, j] = kernel[i, j] * multiplied[i, j];
                 }
             }
-            return Tuple.Create(multipliedR, multipliedG, multipliedB);
+
+            return SumDistance(distance, size);
         }
-        static PixelRGB[,] GetWindow(PixelRGB[,] src, int x1, int x2, int y1, int y2) {
+
+        static int[,] SubstractWindows(byte[,] W1, byte[,] W2, int size) {
+            int[,] result = new int[size, size];
+
+            for(int i = 0; i < size; i++) {
+                for(int j = 0; j < size; j++) {
+                    result[i, j] = W1[i, j] - W2[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        static int[,] MultiplyWindows(int[,] W1, int[,] W2, int size) {
+
+            int[,] result = new int[size, size];
+
+            for(int i = 0; i < size; i++) {
+                for(int j = 0; j < size; j++) {
+                    result[i, j] = W1[i, j] * W2[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        static double SumDistance(double[,] distance, int size) {
+            double sum = 0;
+
+            for(int i = 0; i < size; i++) {
+                for(int j = 0; j < size; j++) {
+                    sum += distance[i, j];
+                }
+            }
+
+            return sum;
+        }
+
+        static byte[,] GetWindow(PixelRGB[,] src, int x1, int x2, int y1, int y2) {
 
             int size1 = x2 - x1;
             int size2 = y2 - y1;
-            PixelRGB[,] window = new PixelRGB[size1, size2];
+            byte[,] window = new byte[size1, size2];
 
             for (int x = x1, i = 0; x < x2; x++, i++) {
                 for (int y = y1, j = 0; y < y2; y++, j++) {
-                    window[i, j] = new PixelRGB(src[x, y].R, src[x, y].G, src[x, y].B);
+                    window[i, j] = src[x, y].I;
                 }
             }
 
--- helpers/GuassianKernelHelper.cs
+++ helpers/GuassianKernelHelper.cs
@@ -5,15 +5,13 @@ namespace DinosaurGraphics.helpers {
         public static double[,] GuassianKernel(int len, double sigma) {
             double[,] gaussianKernel = new double[len, len];
             double sum = 0;
-            int r = len / 2;
-
-            double distance = 0;
+            int r = (len - 1)/2;
 
             double euler = 1f / (2f * Math.PI * Math.Pow(sigma, 2));
 
             for (int y = -r; y <= r; y++) {
                 for (int x = -r; x <= r; x++) {
-                    distance = ((x * x) + (y * y) / (2 * sigma * sigma));
+                    double distance = ((x * x) + (y * y) / (2 * sigma * sigma));
 
                     gaussianKernel[y + r, x + r] = euler * Math.Exp(-distance);
                     sum += gaussianKernel[y + r, x + r];
